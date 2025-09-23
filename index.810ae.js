2040a92d79c53d1aa63e2fa9903f07aa2ab7caff
Alex <92122843+Taduar2001@users.noreply.github.com>
2025-09-23 20:50:30 +0300

    Update

--- index.810ae.js
+++ index.810ae.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.ea986.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -15,12 +15,9 @@ System.register(["./application.js"], function (_export, _context) {
       bcr = $p.getBoundingClientRect();
       canvas.width = bcr.width;
       canvas.height = bcr.height;
-
       if (!('ontouchstart' in window) && !navigator.maxTouchPoints) {
         canvas.focus();
       }
-
-      
       application = new Application();
       topLevelImport('cc').then(function (engine) {
         return application.init(engine);
